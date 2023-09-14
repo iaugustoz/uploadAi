e38a9dbbc2ab3ebf7c3bfd86f6d1323e17b5cd29
Igor Augusto <igoraugustobz@gmail.com>
2023-09-14 15:46:02 -0300

    feat(componentes): Adiciona novos componentes ao projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { FileVideo, Github, Upload, Wand2 } from 'lucide-react';
+import { Button } from "./components/ui/button";
+import { Separator } from './components/ui/separator';
+import { Textarea } from './components/ui/textarea';
+import { Label } from './components/ui/label';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './components/ui/select';
+import { Slider } from './components/ui/slider';
+
 export function App() {
   return (
-    <div></div>
+    // Body do site
+    <div className='min-h-screen flex flex-col'>
+    </div>
   )
 }
